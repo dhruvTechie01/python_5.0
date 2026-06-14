b6789f64d587eed2297c83748111b97f4e3fcfb6
dhruvTechie01 <uniquesdhruv01@gmail.com>
2026-06-14 09:21:54 +0530

    Add initial Python practice code

---
+++ dhruv practice.py
@@ -0,0 +1,151 @@
+# our first program 
+
+
+# print("hello world")
+
+   # used type in python 
+# name="dhruv"
+# print(type(name))
+  
+
+
+                 #take input from user
+  # a=int(input("enter"))
+
+# vol = 4*3.14*(a**3)
+# print(vol)
+
+# dhruv = (input("enter your name:"))
+# dhruv_1 = int(input("enter your age :"))
+
+# print(dhruv)
+# print(dhruv_1)
+
+# # variables 
+
+# name= dhruv 
+# age = 25
+
+# print(name) 
+# print(age)
+
+
+
+              #print in string or out string
+# name = "shardha "
+# age=25
+# price=19.99
+
+# print("my name is :",name)
+# print("my age is:", age)
+# print("my price is :",price)
+
+# print("welcome", name)
+
+# name="sumit"
+# age=24
+# print(type(name))
+# print(type(age))
+
+
+
+                  #data types
+
+# age=24
+# old= False
+# a=None
+# print(type(old))
+# print(type(a))
+
+# ques= print sum of two numbers?
+
+# a=30
+# b=20
+# sum=a+b 
+# print(sum)
+#differece in two numbers
+# diff=a-b
+# print(diff)
+
+# single line comment
+# val=int(input("enter some value:"))
+# print(type(val))
+
+                # #arithmetic operator
+
+# a=5
+# b=2
+  
+# print(a-b)
+
+# print(a+b)
+
+# print(a*b)
+
+# print(a/b) #divison
+
+# print(a%b) #remainder
+
+# print(a**b) #a^b
+
+                  # relational operator
+
+# a=50
+# b=20
+
+# print(a == b) #false # always given boolean value
+# print(a != b) #True
+# print(a >= b) #True
+# print(a <= b ) #False
+
+                 # assignment operator
+
+# num=10
+# num += 10 #10+10=20
+# print("num=",num)
+
+# num=50
+# num*=50
+# print("second number is=",num)
+
+# #logical operator
+
+# a=50
+# b=30
+# print(not False)
+# print(not (a > b) )
+
+              # #control flow # if else
+
+
+# age=18
+
+# if age>=18:
+#     print("you can vote")
+# else:
+#     print("you can not vote")
+
+
+
+#              # loops for loops # repeat code again and again
+
+# for i in range(2,11,2):
+#         print(i)
+
+
+
+               # functions. # code ko reusable krna 
+
+def greet(name):
+                print("hello",name)
+                greet("dhruv")
+
+
+                
+
+         
+
+ 
+
+
+
